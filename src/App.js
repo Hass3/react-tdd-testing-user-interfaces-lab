a93fd75d80de1e0e46e7e2751610f13a66f7620f
Hass <hassmroueh@gmail.com>
2024-10-23 00:57:39 -0400

    react ttd lab complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>Hi, i'm</h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgIbRcFybjyCQ-piEQGGRNFxzLnv1Ps3p1SA&s" alt="Hello"/>
+    <h2>About Me</h2>
+    <p>this is my biography</p>
+    <a href="https://github.com/Hass3/react-tdd-testing-user-interfaces-lab">Git Hub</a>
+    <a href="https://www.linkedin.com/">linked in</a>
+  </div>;
+
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,53 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _____`", ()=>{
+    render(<App />);
+    const topLevelHeading = screen.getByRole("heading", {
+        name : /hi, i'm/i,
+        exact: false,
+        level:1,
+    });
+
+    expect(topLevelHeading).toBeInTheDocument();
+})
+
+ test("image of me with an alt text identifying the content of the image", ()=>{
+    render(<App />);
+    const img = screen.getByAltText("Hello")
+    expect(img).toBeInTheDocument()
+ })
+
+
+ test("displays a second-level heading with the text `About Me`", ()=>{
+    render(<App/>)
+    const secondLevelHeading = screen.getByRole("heading", {
+        name : /about me/i,
+        level:2,
+    });
+    expect(secondLevelHeading).toBeInTheDocument();
+ })
+
+ test("displays a paragraph with my biography", ()=>{
+    render(<App />)
+    const paragraph = screen.getByText("this is my biography")
+    expect(paragraph).toBeInTheDocument();
+ })
+
+ test("Link to my GitHub page", ()=>{
+   render(<App />)
+   const linkElement = screen.getByRole("link", { name: /Git Hub/i })
+   expect(linkElement).toBeInTheDocument();
+   expect(linkElement).toHaveAttribute("href","https://github.com/Hass3/react-tdd-testing-user-interfaces-lab")
+ })
+
+ test("Link to my LinkedIn page",()=>{
+    render(<App />)
+    const linkElement = screen.getByRole("link", { name: /linked in/i })
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute("href","https://www.linkedin.com/")
+ })
+/*
+
+
+Two links, one to your GitHub page, and one to your LinkedIn page */
